784def1ef03a4fccda678ec51b8dd2dd2ddc0396
seabelis <seabelis74@gmail.com>
2019-10-18 21:02:38 +0200

    Throuh step 4

--- messages-api.js
+++ messages-api.js
@@ -1,4 +1,22 @@
+// Create a new JS file named messages-api.js.
+// Create an Express app in that file. The app should listen for requests on port 3000. Make sure you add the required dependency.
+
 const express = require('express')
 const app = express()
 const port = 3000
+// In order to parse the JSON body of the request, you will need to add the middleware for it. Make sure you add the required dependency.
+bodyParser = require('body-parser');
+app.get('/', (req, res) => res.send('tada'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(bodyParser.json());
+
+// Add a single endpoint to the app responds to POST requests to the /messages URI.
+// When a request is sent to the endpoint, it should log the text property of the body to the console, and it should respond with a JSON object
+// test with http -v POST http://localhost:3000/messages
+app.post('/messages', (req, res) => {
+  console.log(req.body)
+  res.json({
+      message: "Message received loud and clear"
+  })
+})
+
