3ca70f9edb52ce93fc11fd92d575b7de513a18a5
seabelis <seabelis74@gmail.com>
2019-10-19 15:45:54 +0200

    cleaned up comments

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -13,18 +13,13 @@ const Movie = sequelize.define('movie', {
 })
 
 //Make sure the model is synched with the database upon startup.
-sequelize .sync()
-
-/////////////TEST//////////////////////
-// .then(() => Movie.create(
-//   { title: 'Citizen Kane', yearOfRelease: 1941, synopsis: "Following the death of a publishing tycoon, news reporters scramble to discover the meaning of his final utterance."},
-//   {title: 'The Godfather', yearOfRelease: 1972, sync: "Francis Ford Coppola''s epic features Marlon Brando in his Oscar-winning role as the patriarch of the Corleone family. Director Coppola paints a chilling portrait of the Sicilian clan''s rise and near fall from power in America, masterfully balancing the story between the Corleone''s family life and the ugly crime business in which they are engaged. Based on Mario Puzo''s best-selling novel and featuring career-making performances by Al Pacino, James Caan and Robert Duvall, this searing and brilliant film garnered ten Academy Award nominations, and won three including Best Picture of 1972. [Paramount Pictures]"},
-//   {title: 'Rear Window', yearOfRelease: 1954, synopsis: "A wheelchair-bound photographer spies on his neighbours from his apartment window and becomes convinced one of them has committed murder."}
-//   ))
-//////////////////////////////////////
+sequelize 
+// .sync()
+.sync({ force: true })
 
 //Use the model create() method to insert 3 rows of example data. This logic should happen after the model synchronization completes. The data should persist. Restarting the API should not cause any data to be lost.
 .then(() => {
+  console.log('Database schema has been updated.');
   const movies = [
   { title: 'Citizen Kane', yearOfRelease: 1941, synopsis: "Following the death of a publishing tycoon, news reporters scramble to discover the meaning of his final utterance."},
   {title: 'The Godfather', yearOfRelease: 1972, synopsis: "Francis Ford Coppola''s epic features Marlon Brando in his Oscar-winning role as the patriarch of the Corleone family. Director Coppola paints a chilling portrait of the Sicilian clan''s rise and near fall from power in America, masterfully balancing the story between the Corleone''s family life and the ugly crime business in which they are engaged. Based on Mario Puzo''s best-selling novel and featuring career-making performances by Al Pacino, James Caan and Robert Duvall, this searing and brilliant film garnered ten Academy Award nominations, and won three including Best Picture of 1972. [Paramount Pictures]"},
@@ -33,4 +28,9 @@ sequelize .sync()
   const moviePromises = movies.map((movie) => Movie.create(movie))
   return Promise.all(moviePromises)})
 
-//Use the model create() method to insert 3 rows of example data. This logic should happen after the model synchronization completes. The data should persist. Restarting the API should not cause any data to be lost.
+//Create an express app with routes that support the following RESTful actions on the "movies" resources.
+// create a new movie resource
+// read all movies (the collections resource)
+// read a single movie resource
+// update a single movie resource
+// delete a single movie resource
