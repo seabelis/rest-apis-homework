ca3c3035141c939063257f3c6cdb12200b983e3a
seabelis <seabelis74@gmail.com>
2019-10-19 10:10:16 +0200

    added validation, but not working

--- messages-api.js
+++ messages-api.js
@@ -11,12 +11,22 @@ app.use(bodyParser.json());
 
 // Add a single endpoint to the app responds to POST requests to the /messages URI.
 // When a request is sent to the endpoint, it should log the text property of the body to the console, and it should respond with a JSON object
+// Perform the following validation: if the body does NOT have a text property or the string is empty, then send a "Bad Request" HTTP status code to the client.
 // test with http -v POST http://localhost:3000/messages message=hi
 
 app.post('/messages', (req, res) => {
   console.log(req.body);
-  res.json({
+  // To check isText seems to require a new library, which is not permitted for this assignment. Is assignment asking to check if null?
+  // if (req.body === null || req.body==='')
+  // (!(req.body).isString || req.body === '') 
+  if (req.body === null || req.body === '') {
+    res
+      .status(500)
+  } else {
+    res.json({
       message: "Message received loud and clear"
-  })
+    })
+  }
+  
+  
 })
-
